b8efb214cecf4428295dd8b70f3064c690012df2
pranasri812 <93368967+pranasri812@users.noreply.github.com>
2023-01-24 10:51:06 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="pranasri812@gmail.com">pranasri812@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Hyderabad - 500001</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:7799889888">7799889888</a>
+      </div>
+
+   </section>
+
+  
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
